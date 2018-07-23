221dbb484787a69631dfbfe411581d8550679852
Viktor <v.melnychuk12@gmail.com>
2018-07-23 18:55:38 +0300

    reactive form for login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
@@ -55,6 +55,7 @@ const appRoutes: Routes = [
     }),
     HttpClientModule,
     FormsModule,
+    ReactiveFormsModule,
     NgbModule.forRoot()
   ],
   providers: [
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -2,35 +2,42 @@ import { Component, OnInit } from '@angular/core';
 import { LoginService } from '../../services/login.service';
 import { MessagesService } from '../../services/messages.service';
 import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  username: string;
-  password: string;
   formValid = true;
+  loginForm: FormGroup;
   constructor(private loginService: LoginService,
-    private msgs: MessagesService, private router: Router) { }
+    private msgs: MessagesService, private router: Router, private fb: FormBuilder) { }
 
   ngOnInit() {
+    this.loginForm = this.fb.group({
+      username: ['', [Validators.required]],
+      password: ['']
+    });
   }
 
   onSubmit() {
-    if (!this.username || !this.password) {
-      this.msgs.showAlert({ severity: 'danger', text: 'Incorrect credentials', module: 'login' });
-      return;
+    if (this.loginForm.valid) {
+      this.loginService.login(this.loginForm.value.username, this.loginForm.value.password).subscribe(resp => {
+        if (resp.accessToken) {
+          localStorage.setItem('access_token', `Bearer ${resp.accessToken}`);
+          this.router.navigate(['/transactions']);
+        } else {
+          return;
+        }
+      });
+    } else {
+
     }
-    this.formValid = true;
-    this.loginService.login(this.username, this.password).subscribe(resp => {
-      if (resp.accessToken) {
-        localStorage.setItem('access_token', `Bearer ${resp.accessToken}`);
-        this.router.navigate(['/transactions']);
-      } else {
-        return;
-      }
-    });
+  }
+  isControlInvalid(control) {
+    const ctrl = this.loginForm.controls[control];
+    return ctrl.invalid && !ctrl.pristine;
   }
 
 }
